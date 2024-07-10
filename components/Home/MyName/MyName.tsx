682cd22d57687d81e04e175922d82c921d4f5035
Muhammad Zen Wawazer <wawazer@gmail.com>
2024-07-10 14:16:17 +0700

    update cv

--- components/Home/MyName/MyName.tsx
+++ components/Home/MyName/MyName.tsx
@@ -106,7 +106,7 @@ export default function MyName(props: { finishedLoading: boolean }) {
         }}
         className="mt-12"
       >
-        <a href={"/resume.pdf"} target={"_blank"} rel="noreferrer">
+        <a href={"/resume2024.pdf"} target={"_blank"} rel="noreferrer">
           <button className="bg-AAprimary text-AAsecondary border rounded px-4 sm:px-8 py-3 sm:py-4 border-AAsecondary">
             Check out my resume!
           </button>
